7f35dbffbe0b084215be1b8bee321d883d57e899
DollaR <cooler84@ukr.net>
2019-05-29 17:36:41 +0300

    create directory dialogs with dialogs module and settings dialog interface

---
+++ dialogs/settings.py
@@ -0,0 +1,29 @@
+"""
+Graphic form change settings.
+
+Created on 29.05.2019
+
+@author: Ruslan Dolovanyuk
+
+"""
+
+import wx
+
+
+class SettingsDialog(wx.Dialog):
+    """Create interface settings dialog."""
+
+    def __init__(self, parent, config):
+        """Initialize interface."""
+        super().__init__(parent, wx.ID_ANY, 'Настройки')
+        self.config = config
+
+        but_save = wx.Button(self, wx.ID_OK, 'Сохранить')
+        but_cancel = wx.Button(self, wx.ID_CANCEL, 'Отмена')
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer_but = wx.GridSizer(rows=1, cols=2, hgap=5, vgap=5)
+        sizer_but.Add(but_save, 0, wx.ALIGN_LEFT | wx.ALIGN_CENTER_VERTICAL)
+        sizer_but.Add(but_cancel, 0, wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL)
+        sizer.Add(sizer_but, 0, wx.EXPAND | wx.ALL)
+        self.SetSizer(sizer)
