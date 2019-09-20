450299a4ffa1954ceadd2eb8f5fbc177a62c964b
DollaR <cooler84@ukr.net>
2019-09-20 21:06:51 +0300

    fix needed commands for compile pyinstaller to correct code

--- install.cmd
+++ install.cmd
@@ -1,12 +1,6 @@
 pyinstaller -F --noconsole ^
---add-binary dialogs/dialogs.py;dialogs ^
---add-binary dialogs/settings.py;dialogs ^
 --add-binary api.pyd;. ^
---add-binary commands.py;. ^
---add-binary configs.py;. ^
---add-binary database.py;. ^
---add-binary drawer.py;. ^
+--add-binary menu.pyd;. ^
 --add-binary tree.pyd;. ^
---add-binary wxdb.py;. ^
 --hidden-import wx ^
 main.pyw
