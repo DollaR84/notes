f89f59759765da9c4ddaf7df491c87a719755b3c
DollaR <cooler84@ukr.net>
2020-05-19 23:58:19 +0300

    fix converter for new optimise tables

--- converter.py
+++ converter.py
@@ -9,6 +9,8 @@ Created on 19.04.2020
 
 import os
 
+from datetime import datetime
+
 from database import Database
 
 import tables
@@ -26,20 +28,19 @@ class DBConverter:
                                    'update_db2',
                                   ]
 
-    def __get_old_data(self, tables):
+    def __get_old_data(self, tables_list):
         """Get all data from old database."""
-        self.__old_data = {table: self.__db.get("SELECT * FROM %s" % table) for table in tables}
+        self.__old_data = {table: self.__db.get("SELECT * FROM %s" % table) for table in tables_list}
 
-    def checker(self, db):
+    def checker(self, db, tables_dict):
         """Check and return version input database."""
         tables_db = db.get_tables_names()
-        tables_cr = tables.get_tables_names()
-        tables_cr.remove('settings')
+        tables_cr = tables.get_tables_names(tables_dict)
         diff_tables = list(set(tables_cr) - set(tables_db))
         if not diff_tables:
             for table in tables_cr:
                 columns_db = db.get_columns_names(table)
-                diff_columns = list(set(tables.get_columns_names(table)) - set(columns_db))
+                diff_columns = list(set(tables.get_columns_names(tables_dict[table])) - set(columns_db))
                 if 'order_sort' in diff_columns:
                     return 1
                 else:
@@ -65,16 +66,17 @@ class DBConverter:
         self.__db.connect(self.__db_name + '.db')
         self.__get_old_data(self.__db.get_tables_names())
         self.__db.disconnect()
-        os.rename(self.__db_name + '.db', self.__db_name + '_old_v1.db')
+        date = datetime.strftime(datetime.now(), "%d.%m.%Y")
+        os.rename(''.join([self.__db_name, '.db']), ''.join([self.__db_name, '.v1.', date, '.db']))
         self.__db.connect(self.__db_name + '.db')
         counter = {}
         for table, rows in self.__old_data.items():
             script = 'CREATE TABLE {} ({}) WITHOUT ROWID'.format(table,
-                ', '.join([' '.join(row) for row in tables.TABLES[table]]))
+                ', '.join([' '.join(row) for row in tables.NOTES[table]]))
             self.__db.put(script)
             for row in rows:
                 row = self.__fix_data(row)
-                columns = tables.get_columns_names(table)
+                columns = tables.get_columns_names(tables.NOTES[table])
                 if table == 'notes':
                     parent = row[-1]
                     if parent not in counter:
@@ -92,13 +94,17 @@ class DBConverter:
         self.__db.commit()
         self.__db.disconnect()
 
-    def run(self):
+    def run(self, tables_dict):
         """Run convert data from old database to new."""
-        self.__db.connect(self.__db_name + '.db')
-        db_ver = self.checker(self.__db)
-        self.__db.disconnect()
-        for index in range(db_ver-1, tables.VERSION-1):
-            getattr(self, self.__update_functions[index])()
+        try:
+            self.__db.connect(self.__db_name + '.db')
+            db_ver = self.checker(self.__db, tables_dict)
+            self.__db.disconnect()
+            for index in range(db_ver-1, tables.VERSION-1):
+                getattr(self, self.__update_functions[index])()
+        except:
+            return False
+        return True
 
 
 def main():
