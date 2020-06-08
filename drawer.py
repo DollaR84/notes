29289337e8efee0fedaadcf1dcd0f610d368820b
DollaR <cooler84@ukr.net>
2020-06-08 18:26:54 +0300

    add widgets for states

--- drawer.py
+++ drawer.py
@@ -7,6 +7,8 @@ Created on 25.05.2019
 
 """
 
+from accessible import TreeAccessible
+
 from commands import Commands
 
 import configs
@@ -55,6 +57,8 @@ class NotesFrame(wx.Frame):
         self.but_save.Disable()
         self.but_del.Disable()
         self.readonly.Disable()
+        self.state_check.Disable()
+        self.states.Disable()
 
     def __create_widgets(self):
         """Create widgets program."""
@@ -64,10 +68,14 @@ class NotesFrame(wx.Frame):
                                 wx.TR_LINES_AT_ROOT |
                                 wx.TR_TWIST_BUTTONS |
                                 wx.TR_EDIT_LABELS)
+        self.tree.SetAccessible(TreeAccessible(self))
         box_note = wx.StaticBox(self.panel, wx.ID_ANY, self.phrases.widgets.box_note)
         self.data = wx.TextCtrl(box_note, wx.ID_ANY, style=wx.TE_MULTILINE)
         self.data.SetBackgroundColour('Yellow')
         self.readonly = wx.CheckBox(self.panel, wx.ID_ANY, self.phrases.widgets.readonly)
+        box_state = wx.StaticBox(self.panel, wx.ID_ANY, self.phrases.widgets.box_state)
+        self.state_check = wx.CheckBox(box_state, wx.ID_ANY, self.phrases.widgets.state_check)
+        self.states = wx.Choice(box_state, wx.ID_ANY, choices=self.config.get_states(self.phrases.widgets.states))
         self.but_save = wx.Button(self.panel, wx.ID_ANY, self.phrases.widgets.but_save)
         self.but_del = wx.Button(self.panel, wx.ID_ANY, self.phrases.widgets.but_del)
         self.but_create = wx.Button(self.panel, wx.ID_ANY, self.phrases.widgets.but_create)
@@ -80,6 +88,10 @@ class NotesFrame(wx.Frame):
         box_note_sizer.Add(self.data, 1, wx.EXPAND | wx.ALL, 5)
         right_sizer.Add(box_note_sizer, 1, wx.EXPAND | wx.ALL)
         right_sizer.Add(self.readonly, 0, wx.EXPAND | wx.ALL | wx.ALIGN_LEFT, 5)
+        state_sizer = wx.GridSizer(rows=1, cols=2, hgap=5, vgap=5)
+        state_sizer.Add(self.state_check, 1, wx.ALIGN_LEFT | wx.ALIGN_CENTER_VERTICAL)
+        state_sizer.Add(self.states, 1, wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL)
+        right_sizer.Add(state_sizer, 0, wx.EXPAND | wx.ALL)
         but_sizer = wx.GridSizer(rows=1, cols=3, hgap=5, vgap=5)
         but_sizer.Add(self.but_save, 0, wx.ALIGN_LEFT | wx.ALIGN_CENTER_VERTICAL)
         but_sizer.Add(self.but_del, 0, wx.ALIGN_CENTER | wx.ALIGN_CENTER_VERTICAL)
@@ -97,6 +109,8 @@ class NotesFrame(wx.Frame):
         self.Bind(wx.EVT_TREE_END_LABEL_EDIT, getattr(self.command, 'tree_end_edit'), self.tree)
         self.Bind(wx.EVT_TEXT, getattr(self.command, 'text_change'), self.data)
         self.Bind(wx.EVT_CHECKBOX, getattr(self.command, 'change_readonly'), self.readonly)
+        self.Bind(wx.EVT_CHECKBOX, getattr(self.command, 'change_state'), self.state_check)
+        self.Bind(wx.EVT_CHOICE, getattr(self.command, 'choice_state'), self.states)
         self.Bind(wx.EVT_BUTTON, getattr(self.command, 'save'), self.but_save)
         self.Bind(wx.EVT_BUTTON, getattr(self.command, 'delete'), self.but_del)
         self.Bind(wx.EVT_BUTTON, getattr(self.command, 'create'), self.but_create)
